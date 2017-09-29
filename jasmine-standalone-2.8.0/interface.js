ed84f33e91d99631c17632a2d380b34d7a585d63
Marcus Rands <mrands@hotmail.co.uk>
2017-09-29 13:16:43 +0100

    Added weather API and incorported in to page

--- jasmine-standalone-2.8.0/interface.js
+++ jasmine-standalone-2.8.0/interface.js
@@ -30,6 +30,18 @@ $(document).ready(function() {
     updateTemperature();
   })
 
+  $.get('http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
+    $('#current-temperature').text(data.main.temp);
+  })
+
+  displayWeather('London');
+
+  $('#select-city').submit(function(event) {
+    event.preventDefault();
+    var city = $('#current-city').val();
+    displayWeather(city);
+  })
+
 function updateTemperature() {
   $('#temperature').text(thermostat.temperature);
   if(thermostat.energyUsage() === 'low-usage') {
@@ -39,6 +51,15 @@ function updateTemperature() {
   } else {
     $('#temperature').css('color', 'red')
   }
+}
+
+function displayWeather(city) {
+  var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
+  var token = '&appid=ba2200495058d48f8c3ab5402610d726';
+  var units = '&units=metric';
+  $.get(url + token + units, function(data) {
+    $('#current-temperature').text(data.main.temp);
+  });
 }
 
 });
